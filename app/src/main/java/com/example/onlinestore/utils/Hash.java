115487d25019c8763d040922f6a3d997e9edebdb
Muhammad Nasir Aziz <nasiraziz08@gmail.com>
2021-03-26 18:58:38 +0500

    Added JazzCash payment gateway

---
+++ app/src/main/java/com/example/onlinestore/utils/Hash.java
@@ -0,0 +1,35 @@
+package com.example.onlinestore.utils;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Hash {
+
+    public static String php_hash_hmac(String data, String secret) {
+        String returnString = "";
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+            byte[] res = sha256_HMAC.doFinal(data.getBytes());
+            returnString = bytesToHex(res);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return returnString;
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        final char[] hexArray = "0123456789abcdef".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0, v; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+}
